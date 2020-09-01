c14431c80691a8e078ae7889415f7b52eb9a9278
lilei <random2035@qq.com>
2020-09-01 16:46:44 +0800

    9

--- gorm/base.go
+++ gorm/base.go
@@ -6,7 +6,7 @@ var db *gorm.DB
 
 func startDB() {
 	var err error
-	db, err = gorm.Open("mysql", "test_u:1918ddkk@(114.115.216.44:33061)/test?charset=utf8mb4&parseTime=True&loc=Local")
+	db, err = gorm.Open("mysql", "test_u:1918ddk@(114.115.216.44:33061)/test?charset=utf8mb4&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
--- gorm/sql_func_test.go
+++ gorm/sql_func_test.go
@@ -19,7 +19,7 @@ func TestSqlFunc(t *testing.T) {
 	// 这种也是可以的
 	//err := db.Raw("select sum(id) as sum from student where id > 0").Pluck("sum", &ss).Error
 
-	err := db.Model(Student{}).Select("sum(id) as sum").Where("id > 0").Pluck("sum", &ss).Error
+	err := db.Debug().Model(Student{}).Select("sum(id) as sum").Where("id > 0").Pluck("sum", &ss).Error
 
 	log.Println(err, ss) // nil, []120
 }
