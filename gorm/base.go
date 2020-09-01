32518bbce0db719e185e1816829639f11c4d7ec9
lilei <random2035@qq.com>
2020-09-01 16:58:17 +0800

    9

--- gorm/base.go
+++ gorm/base.go
@@ -1,12 +1,15 @@
 package main
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+)
 
 var db *gorm.DB
 
 func startDB() {
 	var err error
-	db, err = gorm.Open("mysql", "test_u:1918ddk@(114.115.216.44:33061)/test?charset=utf8mb4&parseTime=True&loc=Local")
+	//  "user:pass@(host:port)/DBNAME?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err = gorm.Open("mysql", GetMysqlConf())
 	if err != nil {
 		panic(err)
 	}
