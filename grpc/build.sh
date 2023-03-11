e3d465b292093e22bb14b3e9e990b1680f4267a1
lei <random2035@qq.com>
2023-03-11 23:04:50 +0800

    rename include

--- grpc/build.sh
+++ grpc/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-mkdir -p pb_gencode/
-
-
 # protoc v3.5.1
 # protoc-gen-go v1.5.3
 # 注意这里不需要指定输出位置，因为全路径已经在所有proto文件中定义好
-protoc -I=./pb --go_out=plugins=grpc:. $(find ./pb/protocol/ -name '*.proto')
+protoc -I=./pb -I=./pb/protoc-v3.5.1 --go_out=plugins=grpc:. $(find ./pb/protocol/ -name '*.proto')
